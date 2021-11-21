4892b707f24234be175fc75de707166878265009
greg <gregory.diamos@gmail.com>
2021-11-21 03:32:32 -0800

    BUG: Stop server path.

--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,3 +1,3 @@
-docker-compose -f ./scripts/docker-compose.yml down
+docker-compose -f deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/scripts/docker-compose.yml down
 docker system prune -f
 exit 0
